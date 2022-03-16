f69a959cc34090950b2b293c125218c7a54eeac9
walterlbf <walter.lbf.f@gmail.com>
2022-03-15 20:18:03 -0400

    svg trocado para brando

--- pages/componentes/Header.jsx
+++ pages/componentes/Header.jsx
@@ -13,6 +13,7 @@ export default function Header() {
         <nav className="hidden lg:flex"> nav </nav>
         <button type="button" className="w-10 h-10 border rounded-md p-2 inline-flex items-center justify-center text-gray-50 lg:hidden focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
           <Image src={menu} width={24} height={24} className="fill-none" />
+          {/* <Menu /> */}
         </button>
       </div>
     </header>
