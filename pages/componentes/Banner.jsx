c8e46aada0a5dd325390910b501100d5f2c5cc87
walterlbf <walter.lbf.f@gmail.com>
2022-03-18 21:17:53 -0400

    add imagem e arredondado botao

--- pages/componentes/Banner.jsx
+++ pages/componentes/Banner.jsx
@@ -1,32 +1,36 @@
 import Image from 'next/image';
 import React from 'react';
 import banner from '../../imagens/TRACTIAN_HOMEPAGE_ANIM.svg';
+import ondas from '../../imagens/ondas.svg';
 
 export default function Banner() {
   return (
-    <section className="text-white container justify-center px-6 pt-36 lg:flex">
-      <div className="container">
-        <h1 className="text-xl font-semibold mr-5 mb-5">
-          Monitoramento Online e Gestão de Ativos em um só lugar
-        </h1>
-        <p className="mb-5 font-light">
-          Evite falhas nas suas máquinas e torne o tempo de inatividade uma
-          coisa do passado com sistema preditivo da TRACTIAN.
-        </p>
-        <button type="button" className="bg-yellow-500 rounded px-6 py-2">Demonstração</button>
-        <p className="mt-2 font-light">
-          Já é cliente?
-          <a
-            href="https://app.tractian.com/login"
-            target="_blank"
-            className="font-bold ml-1"
-            rel="noreferrer"
-          >
-            Acesse aqui.
-          </a>
-        </p>
-      </div>
-      <Image src={banner} className="w-full h-full" />
-    </section>
+    <>
+      <section className="text-white container justify-center px-6 pt-36 lg:flex">
+        <div className="container">
+          <h1 className="text-xl font-semibold mr-5 mb-5">
+            Monitoramento Online e Gestão de Ativos em um só lugar
+          </h1>
+          <p className="mb-5 font-light">
+            Evite falhas nas suas máquinas e torne o tempo de inatividade uma
+            coisa do passado com sistema preditivo da TRACTIAN.
+          </p>
+          <button type="button" className="bg-yellow-500 rounded px-6 py-2">Demonstração</button>
+          <p className="mt-2 font-light">
+            Já é cliente?
+            <a
+              href="https://app.tractian.com/login"
+              target="_blank"
+              className="font-bold ml-1"
+              rel="noreferrer"
+            >
+              Acesse aqui.
+            </a>
+          </p>
+        </div>
+        <Image src={banner} className="w-full h-full" />
+      </section>
+      <Image src={ondas} />
+    </>
   );
 }
--- pages/componentes/Header.jsx
+++ pages/componentes/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
           <Image src={logo} width={159} height={53} />
         </div>
         <nav className="hidden lg:flex"> nav </nav>
-        <button type="button" className="w-10 h-10 border rounded-full p-2 inline-flex items-center justify-center text-gray-50 lg:hidden focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        <button type="button" className="w-10 h-10 border rounded-full p-2 inline-flex items-center justify-center border-yellow-500 text-gray-50 lg:hidden focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
           <Image src={menu} width={16} height={16} className="fill-none" />
         </button>
       </div>
