5220d5fd9e57de396abc7b835ad92c9947dfceaa
walterlbf <walter.lbf.f@gmail.com>
2022-03-20 14:58:47 -0400

    cores das imagens alterada e pros da tractian feito

--- pages/componentes/Pros.jsx
+++ pages/componentes/Pros.jsx
@@ -7,56 +7,57 @@ import pump from '../../imagens/icon-maintence-pump.svg';
 
 export default function Pros() {
   return (
-    <section>
-      <h1>
-        Por que escolher a Tractian?
+    <section className="px-4 py-12">
+      <h1 className="text-center text-3xl pb-6">
+        Por que escolher a
+        <b> Tractian?</b>
       </h1>
-      <article>
-        <Image src={plataform} width={80} height={70} />
-        <h2>
-          Manutenção Preditiva
-        </h2>
-        <p>
-          Detecção automática de falhas,
-          saúde do ativo em tempo real,
-          confiabilidade e insights automáticos.
-        </p>
-      </article>
-      <article>
-        <Image src={online} width={80} height={70} />
-        <h2>
-          Monitoramento Online
-        </h2>
-        <p>
-          Sensor Tractian coleta dados de vibração,
-          temperatura,
-          horímetro e consumo de energia em tempo real.
-
-        </p>
-      </article>
-      <article>
-        <Image src={pump} width={80} height={70} />
-        <h2>
-          Setup Instantâneo
-        </h2>
-        <p>
-          Solução Plug & Play,
-          sem necessidade de WiFi industrial, comunicação via 3G/4G independente.
-        </p>
-      </article>
-      <article>
-        <Image src={horimeter} width={80} height={70} />
-        <h2>
-          CMMS Integrado
-        </h2>
-        <p>
-          Automatize processos,
-          ordens de serviço e tenha total controle sobre seus ativos e equipe.
-
-        </p>
-      </article>
-      <div className="text-center">
-        <button type="button">Fale com um especialista</button>
+      <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <article className="text border-2 border-yellow-500 rounded px-8 py-12">
+          <Image src={plataform} width={80} height={70} />
+          <h2 className="text-left font-bold text-lg mb-2 mt-2">
+            Manutenção Preditiva
+          </h2>
+          <p className="text-sm">
+            Detecção automática de falhas,
+            saúde do ativo em tempo real,
+            confiabilidade e insights automáticos.
+          </p>
+        </article>
+        <article className="text border-2 border-yellow-500 rounded px-8 py-12">
+          <Image src={online} width={80} height={70} />
+          <h2 className="text-left font-bold text-lg mb-2 mt-2">
+            Monitoramento Online
+          </h2>
+          <p className="text-sm">
+            Sensor Tractian coleta dados de vibração,
+            temperatura,
+            horímetro e consumo de energia em tempo real.
+          </p>
+        </article>
+        <article className="text border-2 border-yellow-500 rounded px-8 py-12">
+          <Image src={pump} width={80} height={70} />
+          <h2 className="text-left font-bold text-lg mb-2 mt-2">
+            Setup Instantâneo
+          </h2>
+          <p className="text-sm">
+            Solução Plug & Play,
+            sem necessidade de WiFi industrial, comunicação via 3G/4G independente.
+          </p>
+        </article>
+        <article className="text border-2 border-yellow-500 rounded px-8 py-12">
+          <Image src={horimeter} width={80} height={70} />
+          <h2 className="text-left font-bold text-lg mb-2 mt-2">
+            CMMS Integrado
+          </h2>
+          <p className="text-sm">
+            Automatize processos,
+            ordens de serviço e tenha total controle sobre seus ativos e equipe.
+          </p>
+        </article>
+      </section>
+      <div className="text-center mt-6">
+        <button type="button" className="bg-yellow-500 rounded font-bold px-6 py-2">Fale com um especialista</button>
       </div>
     </section>
   );
