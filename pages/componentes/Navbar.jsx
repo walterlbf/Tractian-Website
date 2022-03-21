56ee2a6ed1841097d5c6b256ef0110d4cebfac3a
walterlbf <walter.lbf.f@gmail.com>
2022-03-20 21:18:19 -0400

    ajuste de gap

--- pages/componentes/Navbar.jsx
+++ pages/componentes/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className="hidden ml-16 lg:grid text-white grid-cols-2 gap-2 items-center">
-      <ul className="flex gap-4">
+      <ul className="flex gap-6">
         <li>Soluções</li>
         <li>Sobre Nós</li>
         <li>Materiais Gratuitos</li>
         <li>Blog</li>
       </ul>
-      <div className="grid grid-cols-2 pl-5 items-center ml-40">
-        <a href="#">Área do CLiente</a>
+      <div className="flex gap-6 pl-5 items-center ml-32">
+        <a href="/" className=" font-extrabold">Área do CLiente</a>
         <button type="button" className="bg-yellow-500 rounded px-6 py-2">Demonstração</button>
       </div>
     </nav>
